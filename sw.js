b976d8ac46ceb9f86f734d77d9bc662e86c86423
edinjson <figueroaedinson@gmail.com>
2020-12-02 22:39:19 -0500

    finalizado

---
+++ sw.js
@@ -0,0 +1,50 @@
+//Archivos a cachear para usar sin internet
+const nombreCache = 'apv-v1';
+const archivos = [
+'/',
+'index.html',
+'error.html',
+'css/bootstrap.css',
+'css/styles.css',
+'js/app.js',
+'js/apv.js'
+];
+
+
+//Cuando se instala el service worker
+
+self.addEventListener('install', e => {
+    console.log("Instalando el Service Worker");
+    console.log(e);
+
+    e.waitUntil(
+        caches.open(nombreCache)
+            .then( cache =>{
+                console.log("Cacheando...");
+                cache.addAll(archivos);
+            })
+    )
+
+
+
+});
+
+//Actiavar el Service Worker
+self.addEventListener('activate', e =>{
+    console.log("Service Worker activado");
+    console.log(e);
+});
+
+
+//Evento fetch para descargar archivos estaticos
+
+self.addEventListener('fetch', e => {
+    console.log("Fetch...", e);
+
+    e.respondWith(
+        caches
+          .match(e.request)
+          .then(cacheResponse => (cacheResponse ? cacheResponse : caches.match('error.html')))
+        
+      )
+});
